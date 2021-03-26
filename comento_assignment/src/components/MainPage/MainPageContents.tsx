1ddfbef79d0a1382e30fa06ede5fc667d83cd7ff
ning1315 <59202849+ning1315@users.noreply.github.com>
2021-03-27 03:09:38 +0900

    feature OneContentsBox

---
+++ comento_assignment/src/components/MainPage/MainPageContents.tsx
@@ -0,0 +1,30 @@
+const MainPageContents = () => {
+  return (
+    <>
+      <div className="MainPageOneContents">
+        <div className="MainPageOneContents-Top">
+          <div className="MainPageOneContents-Cate">category_name</div>
+          <div className="MainPageOneContents-Id">id</div>
+        </div>
+        <div className="MainPageOneContents-UnderBar"></div>
+        <div className="MainPageOneContents-Info">
+          <div className="MainPageOneContents-Info-Id">user_id</div>
+          <div className="MainPageOneContents-Info-SideBar"></div>
+          <div className="MainPageOneContents-Info-Date">
+            created_at(2020-02-02)
+          </div>
+        </div>
+        <div className="MainPageOneContents-Title">
+          Title Title Title Title Title Title Title Title Title Title Title
+          Title Title Title Title Title Title Tiㅁㄴㅇㅁㄴㅇㅁㄴㅇ
+        </div>
+        <div className="MainPageOneContents-Desc">
+          contents contents contents contents contents contents contents
+          contents contents contents contents con…ㅁㄴㅇㅁㄴㅇㅁㄴㅇ
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default MainPageContents;
