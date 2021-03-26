7186df82ee3259ae88c55217ee3d1479e88fa818
ning1315 <59202849+ning1315@users.noreply.github.com>
2021-03-26 19:46:03 +0900

    feature Header

---
+++ comento_assignment/src/components/head/Header.tsx
@@ -0,0 +1,11 @@
+import { useState, useEffect } from 'react';
+
+const Header = () => {
+  return (
+    <div className="Header-Container">
+      <span className="assignMent-info">[3월 26일] 이상철</span>
+    </div>
+  );
+};
+
+export default Header;
